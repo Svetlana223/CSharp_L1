208cdc0ba0e13bc8e16ce2189180a8ba50447b02
Svetlana <strel_sv@bk.ru>
2022-04-14 13:15:06 +0300

    add example 007_Magic corrected

--- example007_Magic/Program.cs
+++ example007_Magic/Program.cs
@@ -2,9 +2,9 @@
 // Console.SetCursorPosition(10, 4);
 // Console.WriteLine("+");
 
-int xa = 1, ya = 1,
-    xb = 1, yb = 29,
-    xc = 80, yc = 29;
+int xa = 40, ya = 1,
+    xb = 1, yb = 30,
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa, ya);
 Console.WriteLine("+");
@@ -18,9 +18,9 @@ Console.WriteLine("+");
 
 int x = xa, y = xb;
 
-int count = 10;
+int count = 0;
 
-while(count < 10)
+while(count < 10000)
 {
     int what = new Random().Next(0, 3); //[0;3) 0 1 2
     if(what == 0)
